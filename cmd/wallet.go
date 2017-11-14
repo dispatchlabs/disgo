b6f1d2219ecd4fb51f478f7cbca497260f18b561
Avery Hankins <averyhankins@Averys-MacBook-Pro.local>
2017-11-13 18:37:46 -0600

    creation of files to store private key and public key in json file (unfinished)

--- cmd/wallet.go
+++ cmd/wallet.go
@@ -16,12 +16,15 @@ package cmd
 import (
 	"fmt"
 	"io"
+	//"io/ioutil"
 	"crypto/ecdsa"
 	"crypto/elliptic"
 	"crypto/rand"
 	"crypto/x509"
 	"encoding/hex"
-
+	//"encoding/json"
+	"os"
+	"os/user"
 	"github.com/spf13/cobra"
 )
 
@@ -32,11 +35,13 @@ var walletCmd = &cobra.Command{
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
 
+
 Cobra wallets when I tell it to.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("wallet called")
 		fmt.Println()
 
+		fmt.Println("creating keys")
 		private_key, public_key := genPPKeys(rand.Reader)
 		fmt.Print("private key: 0x")
 		fmt.Println(hex.EncodeToString(private_key[:]))
@@ -47,10 +52,69 @@ Cobra wallets when I tell it to.`,
 	},
 }
 
+type Rankings struct {
+    		private_key  string `json:"private_key"`
+    		private_key_bytes string `json:"private_key_bytes"`
+    		public_key_bytes string `json:"public_key_bytes"`
+    	}
+
 func genPPKeys(random io.Reader) (private_key_bytes, public_key_bytes []byte) {
 	private_key, _ := ecdsa.GenerateKey(elliptic.P224(), random)
 	private_key_bytes, _ = x509.MarshalECPrivateKey(private_key)
 	public_key_bytes, _ = x509.MarshalPKIXPublicKey(&private_key.PublicKey)
+	fmt.Println("Checking if there's a dispatch folder")
+	fmt.Println()
+	//Checks if there is a Dispatch folder in the users Home. If not, it creates one.
+	usr, _ := user.Current()
+	var dir = usr.HomeDir
+
+	if _, err := os.Stat(dir + "/disgo"); os.IsNotExist(err) {
+		fmt.Println(os.ModePerm)
+
+		//find user current directory and make the disgo folder inside it
+    	os.Mkdir(dir + "/disgo", os.ModePerm)
+
+    	fmt.Println("creating dispatch folder")
+    	fmt.Println()
+    	fmt.Println("creating file to hold keys")
+    	fmt.Println()
+    	
+    	fmt.Println(dir)
+		fmt.Println("/disgo/do_not_touch.json")
+    	var _, err = os.Stat(dir + "/disgo/do_not_touch.json")
+    	fmt.Println("after os.Stat")
+		// create file if not exists
+		if os.IsNotExist(err) {
+			
+			file, err := os.Create(dir + "/disgo/do_not_touch.json")
+			fmt.Println(err)
+
+			fmt.Println("after os.create")
+			if isError(err) { return }
+			fmt.Println("after if isError")
+			defer file.Close()
+		}
+		fmt.Println("==> done creating file", dir + "/disgo/do_not_touch.json")
+
+		//fmt.Println(hex.EncodeToString(private_key[:]))
+
+
+		//var jsonBlob = []byte(`
+        //{"private_key":"`+hex.EncodeToString(private_key)+`", "private_key_bytes":"`+hex.EncodeToString(private_key_bytes[:])+`","public_key_bytes":"`+hex.EncodeToString(public_key_bytes[:])+`"}`)
+		// // open file using READ & WRITE permission
+		// rankings := Rankings{}
+  //  		err = json.Unmarshal(jsonBlob, &rankings)
+  //   	if err != nil {
+  //      		 //nozzle.printError("opening config file", err.Error())
+  //  		 }
+
+  //   	rankingsJson, _ := json.Marshal(rankings)
+  //   	err = ioutil.WriteFile("~/dispatch/do_not_touch.json", rankingsJson, 0644)
+  //   	fmt.Printf("%+v", rankings)
+	
+		// fmt.Println("==> done writing to file")
+	}
+
 	return private_key_bytes, public_key_bytes
 }
 
@@ -67,4 +131,12 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// walletCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func isError(err error) bool {
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	return (err != nil)
 }
