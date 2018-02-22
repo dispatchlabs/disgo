305ddc72503626f7b4b17dabadf0923ef4923da9
Christopher McGregor <chris@mcgregor.io>
2018-02-21 18:34:36 -0800

    Clean up

--- core/server.go
+++ core/server.go
@@ -58,12 +58,6 @@ func (server *Server) Go() {
 	log.Info("booting Disgo v" + Version + "...")
 	log.Info("args  [" + strings.Join(os.Args, " ") + "]")
 
-
-	foo := &Foo{}
-	json.Unmarshal([]byte(`{"name":"FOOK YOU"}`), foo)
-
-
-
 	// Add services.
 	server.services = append(server.services, dapos.NewDAPoSService())
 	server.services = append(server.services, disgover.NewDisGoverService())
