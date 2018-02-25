b15cafb0f0b108ee72bf2185b4027295a139ad4a
Christopher McGregor <chris@mcgregor.io>
2018-02-25 11:43:17 -0800

    Clean up

--- core/api.go
+++ core/api.go
@@ -10,12 +10,15 @@ import (
 	log "github.com/sirupsen/logrus"
 	"encoding/json"
 	dapos "github.com/dispatchlabs/dapos/core"
+	"github.com/dispatchlabs/disgo_commons/crypto"
+	"time"
+	"encoding/hex"
 )
 
 // Api
 type Api struct {
 	services []types.IService
-	router *mux.Router
+	router   *mux.Router
 }
 
 // NewApi
@@ -31,24 +34,60 @@ func NewApi(services []types.IService) *Api {
 
 // createWalletHandler
 func (this *Api) createWalletHandler(responseWriter http.ResponseWriter, request *http.Request) {
-	/*
-	body, error := ioutil.ReadAll(request.Body)
+
+	// TODO: Remove (just for flushing out API). MAO!
+	walletAccount := types.NewWalletAccount()
+	walletAccount.Balance = 100
+
+	// Write response.
+	response, error := json.Marshal(struct {
+		Status string `json:"status,omitempty"`
+		WalletAccount *types.WalletAccount `json:"walletAccount,omitempty"`
+	}{
+		Status: "OK",
+		WalletAccount: walletAccount,
+	})
 	if error != nil {
 		log.WithFields(log.Fields{
-			"method": "Server.createTransactionHandler",
-		}).Error("unable to read HTTP body of request ", error)
-		http.Error(responseWriter, "error reading HTTP body of request", http.StatusBadRequest)
+			"method": "Server.createWalletHandler",
+		}).Error("unable to create response JSON [error=", error.Error() + "]")
+		http.Error(responseWriter, `{"status":"INTERNAL_SERVER_ERROR"}`, http.StatusInternalServerError)
 		return
 	}
-	*/
-	log.Info("create wallet")
+	responseWriter.Write(response)
+	log.WithFields(log.Fields{
+		"method": "Server.createWalletHandler",
+	}).Info(string(response))
 }
 
 // retrieveWalletHandler
 func (this *Api) retrieveWalletHandler(responseWriter http.ResponseWriter, request *http.Request) {
 	vars := mux.Vars(request)
-	responseWriter.WriteHeader(http.StatusOK)
-	log.Info("retrieve wallet [address=" + vars["wallet_address"] + "]")
+
+	// TODO: Remove (just for flushing out API). MAO!
+	walletAccount := types.NewWalletAccount()
+	address, _  := hex.DecodeString(vars["wallet_address"])
+	copy (walletAccount.Address[:], address)
+
+	// Write response.
+	response, error := json.Marshal(struct {
+		Status string `json:"status,omitempty"`
+		WalletAccount *types.WalletAccount `json:"walletAccount,omitempty"`
+	}{
+		Status: "OK",
+		WalletAccount: walletAccount,
+	})
+	if error != nil {
+		log.WithFields(log.Fields{
+			"method": "Server.retrieveWalletHandler",
+		}).Error("unable to create response JSON [error=", error.Error() + "]")
+		http.Error(responseWriter, `{"status":"INTERNAL_SERVER_ERROR"}`, http.StatusInternalServerError)
+		return
+	}
+	responseWriter.Write(response)
+	log.WithFields(log.Fields{
+		"method": "Server.retrieveWalletHandler",
+	}).Info(string(response))
 }
 
 // createTransactionHandler
@@ -58,7 +97,7 @@ func (this *Api) createTransactionHandler(responseWriter http.ResponseWriter, re
 		log.WithFields(log.Fields{
 			"method": "Server.createTransactionHandler",
 		}).Error("unable to read HTTP body of request ", error)
-		http.Error(responseWriter, "error reading HTTP body of request", http.StatusBadRequest)
+		http.Error(responseWriter, `{"status":"INTERNAL_SERVER_ERROR"}`, http.StatusInternalServerError)
 		return
 	}
 
@@ -67,30 +106,86 @@ func (this *Api) createTransactionHandler(responseWriter http.ResponseWriter, re
 	if error != nil {
 		log.WithFields(log.Fields{
 			"method": "Server.createTransactionHandler",
-		}).Error("JSON_PARSE_ERROR ", error) // TODO: Should return JSON!!!
-		http.Error(responseWriter, "error reading HTTP body of request", http.StatusBadRequest)
+		}).Error("JSON_PARSE_ERROR", error)
+		http.Error(responseWriter, `{"status":"JSON_PARSE_ERROR"}`, http.StatusBadRequest)
 		return
 	}
+
+	// TODO: Remove (just for flushing out API). MAO!
+	transaction.Hash = crypto.NewHash()
+	transaction.Time = time.Now()
 
 	// Create transaction.
 	_, error = this.getService(&dapos.DAPoSService{}).(*dapos.DAPoSService).CreateTransaction(transaction, nil)
 	if error != nil {
 		log.WithFields(log.Fields{
 			"method": "Server.createTransactionHandler",
-		}).Error("JSON_PARSE_ERROR ", error) // TODO: Should return JSON!!!
+		}).Error("JSON_PARSE_ERROR [error=", error.Error()+"]")
 		http.Error(responseWriter, "error reading HTTP body of request", http.StatusBadRequest)
 		return
 	}
 
-	http.Error(responseWriter, "{}", http.StatusOK)
-
+	// Write response.
+	response, error := json.Marshal(struct {
+		Status string `json:"status,omitempty"`
+		Transaction *types.Transaction `json:"transaction,omitempty"`
+	}{
+		Status: "OK",
+		Transaction: transaction,
+	})
+	if error != nil {
+		log.WithFields(log.Fields{
+			"method": "Server.createTransactionHandler",
+		}).Error("unable to create response JSON [error=", error.Error() + "]")
+		http.Error(responseWriter, `{"status":"INTERNAL_SERVER_ERROR"}`, http.StatusInternalServerError)
+		return
+	}
+	responseWriter.Write(response)
+	log.WithFields(log.Fields{
+		"method": "Server.createTransactionHandler",
+	}).Info(string(response))
 }
 
 // retrieveTransactionHandler
 func (this *Api) retrieveTransactionHandler(responseWriter http.ResponseWriter, request *http.Request) {
 	vars := mux.Vars(request)
-	responseWriter.WriteHeader(http.StatusOK)
-	log.Info("retrieve transactions [address=" + vars["wallet_address"] + "]")
+
+	// TODO: Remove (just for flushing out API). MAO!
+	var transactions [2] *types.Transaction
+	transaction := types.NewTransaction()
+	transaction.Value = 2
+	from, _  := hex.DecodeString(vars["wallet_address"])
+	copy (transaction.From[:], from)
+	to, _  := hex.DecodeString("cc3f682246d4a755833f9cb19e1acc8565a0c2ba")
+	copy (transaction.To[:], to)
+	transactions[0] = transaction
+	transaction = types.NewTransaction()
+	transaction.Value = 4
+	from, _  = hex.DecodeString(vars["wallet_address"])
+	copy (transaction.From[:], from)
+	to, _  = hex.DecodeString("cc3f682246d4a755833f9cb19e1acc8565a0c2ba")
+	copy (transaction.To[:], to)
+	transactions[1] = transaction
+
+	// Write response.
+	response, error := json.Marshal(struct {
+		Status string `json:"status,omitempty"`
+		Transactions [2] *types.Transaction `json:"transactions,omitempty"`
+	}{
+		Status: "OK",
+		Transactions: transactions,
+	})
+	if error != nil {
+		log.WithFields(log.Fields{
+			"method": "Server.retrieveTransactionHandler",
+		}).Error("unable to create response JSON [error=", error.Error() + "]")
+		http.Error(responseWriter, `{"status":"INTERNAL_SERVER_ERROR"}`, http.StatusInternalServerError)
+		return
+	}
+	responseWriter.Write(response)
+	log.WithFields(log.Fields{
+		"method": "Server.retrieveTransactionHandler",
+	}).Info(string(response))
 }
 
 // getService
@@ -102,6 +197,3 @@ func (this *Api) getService(serviceInterface interface{}) types.IService {
 	}
 	return nil
 }
-
-
-
--- curls/retrieve_transactions.sh
+++ curls/retrieve_transactions.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-curl 'http://localhost:1975/v1/transactions/aef26adf2dd6a622dbd9c66f34b4722b1c63524bba9dd3d11394c4bcdd7eaf86'
+curl 'http://localhost:1975/v1/transactions/123f681646d4a755815f9cb19e1acc8565a0c2ac'
