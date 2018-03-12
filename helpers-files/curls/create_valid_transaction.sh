8a8b5508e4736abb373419adf64734bcf47e0f5b
Christopher McGregor <chris@mcgregor.io>
2018-03-12 09:18:04 -0700

    Updated curls scripts

--- helpers-files/curls/create_valid_transaction.sh
+++ helpers-files/curls/create_valid_transaction.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-curl -X POST -d '{"hash":"d4076ff453736aad7c495c281e0dc39015adcc8934e4e23acd786238a4934a90","from":"19b1edf2e533fd638bad65bd2644e8af7591b3e7","to":"a0be735d740ed1a79ff1cfbe0a8dc7f53276c64d","value":10,"time":"2018-03-03T17:34:51.617181-08:00","signature":"3fd985992dc395b6918a04c077ff492212bb0db81e6290adf021265d00b429ad7e61b3f1ace73e65fdae440b2b4ed183c6a3661001f9ef0324d6a74109a4122300"}' 'http://localhost:1975/v1/transactions'
+curl -X POST -d '{"hash":"153abd30bbae632dcd8ce47c482a0f5ac7c7fff0e9aec8407f8937bd16f2ae47","from":"428273ddce244807d8dcb135b1657b81fa18e7f5","to":"428273ddce244807d8dcb135b1657b81fa18e7f5","value":10,"time":"2018-03-12T09:05:33.273633-07:00","signature":"f55ef22b42033ac9ca84e68d5decfa9d81f677e059b08cc08e8afb2e516163247fc4d1eed4bdf4735c170725bb982f58cd7219f5a114925cded687d3f9ea0b5b00"}' 'http://localhost:1975/v1/transactions'
