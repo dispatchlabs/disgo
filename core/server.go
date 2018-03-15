80148663b7d8912401dc47ca0f9d056afa2b1f1f
Christopher McGregor <chris@mcgregor.io>
2018-03-14 18:04:05 -0700

    ./config/config.json is ceated if doesn't exist

--- core/server.go
+++ core/server.go
@@ -57,14 +57,31 @@ func NewServer() *Server {
 		"method": utils.GetCallingFuncName() + fmt.Sprintf(" -> %s", utils.GetDisgoDir()),
 	}).Info("config folder")
 
-	var configFile = utils.GetDisgoDir() + string(os.PathSeparator) + "config.json"
-	if utils.Exists(configFile) {
-		file, error := ioutil.ReadFile(configFile)
+	var configFileName = utils.GetDisgoDir() + string(os.PathSeparator) + "config.json"
+	if utils.Exists(configFileName) {
+		file, error := ioutil.ReadFile(configFileName)
 		if error != nil {
-			log.Error("unable to load " + configFile + "[error=" + error.Error() + "]")
+			log.Error("unable to load " + configFileName + "[error=" + error.Error() + "]")
 			os.Exit(1)
 		}
 		json.Unmarshal(file, &config.Properties)
+	} else {
+		file, error := os.Create(configFileName)
+		defer file.Close()
+		if error != nil {
+			log.WithFields(log.Fields{
+				"method": utils.GetCallingFuncName(),
+			}).Fatal("unable to create " + configFileName + " [error=" + error.Error() + "]")
+			panic(error)
+		}
+		bytes, error := json.Marshal(&config.Properties)
+		if error != nil {
+			log.WithFields(log.Fields{
+				"method": utils.GetCallingFuncName(),
+			}).Fatal("unable to Marshal States [error=" + error.Error() + "]")
+			panic(error)
+		}
+		fmt.Fprintf(file, string(bytes))
 	}
 
 	// Load Keys
