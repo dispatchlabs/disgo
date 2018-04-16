ffeae96524dcbfdb02d2edfc1f9d02a387cd97c5
Christopher McGregor <chris@mcgregor.io>
2018-04-16 07:22:28 -0700

    New curls

--- helpers-files/curls/get_status.sh
+++ helpers-files/curls/get_status.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-curl 'http://10.0.1.2:1975/v1/statuses/b67194d0-f329-4686-9a23-a636bf53eb0f'
+curl 'http://35.185.250.184:1975/v1/statuses/b67194d0-f329-4686-9a23-a636bf53eb0f'
