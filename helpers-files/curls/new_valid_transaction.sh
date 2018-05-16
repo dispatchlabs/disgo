9c5168678464539e26a16d6a8e047e8bb1b3751c
Christopher McGregor <chris@mcgregor.io>
2018-05-16 14:26:35 -0700

    Clean up

--- helpers-files/curls/new_valid_transaction.sh
+++ helpers-files/curls/new_valid_transaction.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-curl -X POST -d '{"hash":"40bbe0eaddfcb1af5ef8331ad1c775408ca64c19a631595182a40ffc7acc870a","type":0,"from":"79db55dd1c8ae495c267bde617f7a9e5d5c67719","to":"c296220327589dc04e6ee01bf16563f0f53895bb","value":999,"time":1521753058854,"signature":"f8438426b6ea3350f566b0f62dc1c36148fb844aac5e4b511cb3d7e84962dca13948b4d6ebcc896e222366863de2e44ffb48af21f0d4fe532159053eaa9dc9ea00"}' 'http://35.185.250.184:1975/v1/transactions'
+curl -X POST -d '{"hash":"278827a75df574a7ec1a4ca9e573e4441d61f6f6444fb707b847abe810ef0cb0","type":0,"from":"3ed25f42484d517cdfc72cafb7ebc9e8baa52c2c","to":"d70613f93152c84050e7826c4e2b0cc02c1c3b99","value":999,"code":"","method":"","time":1526494383884,"signature":"74a8babb53caae899998fabce9fa89f68af81924d92f3427d6c261090c8f2c5752eda923b12defb84605a29f84cf62b63864a5b48c2f62d472ed4249cf49608f00","hertz":0,"fromName":"","toName":""}' 'http://10.0.1.2:1975/v1/transactions'
