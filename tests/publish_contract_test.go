6dacaa04bcd5b22d77788e533a02e4e27d7ee29e
nic0lae <nic0lae@linux.com>
2018-05-17 17:43:34 -0700

    Adjusting the contracts test

--- tests/publish_contract_test.go
+++ tests/publish_contract_test.go
@@ -3,24 +3,16 @@ package tests
 import (
 	"fmt"
 	"testing"
+	"time"
 
 	"github.com/dispatchlabs/commons/services"
+	"github.com/dispatchlabs/commons/types"
 	"github.com/dispatchlabs/commons/utils"
 	"github.com/dispatchlabs/dapos"
 	"github.com/dispatchlabs/disgo/core"
 	"github.com/dispatchlabs/disgover"
 	"github.com/dispatchlabs/dvm"
 )
-
-// //privateKey []byte, tipe byte, from, to string, value, hertz, theTime int64
-
-// t, err := types.NewTransaction("0f86ea981203b26b5b8244c8f661e30e5104555068a4bd168d3e3015db9bb25a", 0, "3ed25f42484d517cdfc72cafb7ebc9e8baa52c2c", "d70613f93152c84050e7826c4e2b0cc02c1c3b99", 999, 0, utils.ToMilliSeconds(time.Now()))
-// if err != nil {
-// 	utils.Error(err)
-// }
-
-// utils.Info(t.Verify())
-// utils.Info(t.String())
 
 var nrOfServices = 6
 
@@ -49,4 +41,30 @@ func allServicesInitFinished() {
 	if nrOfServices != 0 {
 		return
 	}
+
+	// {"address":"3ed25f42484d517cdfc72cafb7ebc9e8baa52c2c","privateKey":"0f86ea981203b26b5b8244c8f661e30e5104555068a4bd168d3e3015db9bb25a"}
+
+	var privateKey = "0f86ea981203b26b5b8244c8f661e30e5104555068a4bd168d3e3015db9bb25a"
+	var tipe byte = 0
+	var from = "3ed25f42484d517cdfc72cafb7ebc9e8baa52c2c"
+	var to = "d70613f93152c84050e7826c4e2b0cc02c1c3b99"
+	var value int64 = 999
+	var hertz int64 = 0
+	var theTime int64 = utils.ToMilliSeconds(time.Now())
+
+	t, err := types.NewTransaction(
+		privateKey,
+		tipe,
+		from,
+		to,
+		value,
+		hertz,
+		theTime,
+	)
+	if err != nil {
+		utils.Error(err)
+	}
+
+	utils.Info(t.Verify())
+	utils.Info(t.String())
 }
