60b57147941f598fe82895eac0038e68da045304
Bob Stevens <bob@brightappsllc.com>
2018-07-16 16:53:09 -0700

    Add Ability to sort by time

---
+++ commons/types/transaction_sorter.go
@@ -0,0 +1,48 @@
+package types
+
+import "sort"
+
+// By is the type of a "less" function that defines the ordering of its Planet arguments.
+type By func(t1, t2 *Transaction) bool
+
+// Sort is a method on the function type, By, that sorts the argument slice according to the function.
+func (by By) Sort(transactions []*Transaction) {
+	ts := &TransactionSorter{
+		transactions: transactions,
+		by:      by, // The Sort method's receiver is the function (closure) that defines the sort order.
+	}
+	sort.Sort(ts)
+}
+
+// planetSorter joins a By function and a slice of Planets to be sorted.
+type TransactionSorter struct {
+	transactions 	[]*Transaction
+	by      		func(t1, t2 *Transaction) bool // Closure used in the Less method.
+}
+
+// Len is part of sort.Interface.
+func (ts *TransactionSorter) Len() int {
+	return len(ts.transactions)
+}
+
+// Swap is part of sort.Interface.
+func (ts *TransactionSorter) Swap(i, j int) {
+	ts.transactions[i], ts.transactions[j] = ts.transactions[j], ts.transactions[i]
+}
+
+// Less is part of sort.Interface. It is implemented by calling the "by" closure in the sorter.
+func (ts *TransactionSorter) Less(i, j int) bool {
+	return ts.by(ts.transactions[i], ts.transactions[j])
+}
+
+func SortByTime(txs []*Transaction, ascending bool) {
+	timestamp := func(t1, t2 *Transaction) bool {
+		if(ascending) {
+			return t1.Time < t2.Time
+		} else {
+			return t2.Time < t1.Time
+		}
+	}
+	By(timestamp).Sort(txs)
+}
+
