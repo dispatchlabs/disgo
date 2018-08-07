256f1bf35516ff25ebfa772c27a5308db5c1357a
davedispatch <38048583+davedispatch@users.noreply.github.com>
2018-08-07 10:15:15 -0700

    DG-607 - Adding CreateAccount method to GoSDK (#51)
    * Adding CreateAccount to Go SDK
    
    * Fixing missing type
    
    * DG-607 adding test for createAccount
    
    * Allowing name to be optional in GoSDK.CreateAccount
    
    * Extending GoSDK.CreateAccount unit test
    
    * Fixing GoSDK.CreateAccount unit test

---
+++ sdk/endpoints_test.go
@@ -0,0 +1,22 @@
+package sdk
+
+import (
+	"testing"
+	"fmt"
+)
+
+func TestCreateAccount(t *testing.T) {
+	// Testing WITHOUT name
+	account, err := CreateAccount()
+	if err != nil {
+		t.Error(err)
+	}
+	fmt.Printf("%v\n", account.ToPrettyJson())
+
+	// Testing WITH name
+	account, err = CreateAccount("Test")
+	if err != nil {
+		t.Error(err)
+	}
+	fmt.Printf("%v\n", account.ToPrettyJson())
+}
