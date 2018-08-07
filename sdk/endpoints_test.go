009dbd6e9498bca88d138e9bbdc358f93b35972c
David Hutchings <chefslide@gmail.com>
2018-08-07 09:51:59 -0700

    Extending GoSDK.CreateAccount unit test

--- sdk/endpoints_test.go
+++ sdk/endpoints_test.go
@@ -6,6 +6,14 @@ import (
 )
 
 func TestCreateAccount(t *testing.T) {
+	// Testing WITHOUT name
+	account, err := CreateAccount()
+	if err != nil {
+		t.Error(err)
+	}
+	fmt.Printf("%v\n", account.ToPrettyJson())
+
+	// Testing WITH name
 	account, err := CreateAccount("Test")
 	if err != nil {
 		t.Error(err)
