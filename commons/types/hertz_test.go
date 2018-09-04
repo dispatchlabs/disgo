07346e8f3b28b12105f7a5515034d59536891c1c
Bob Stevens <Bob@Bobs-MacBook-Pro.local>
2018-09-04 10:20:42 -0700

    test

--- commons/types/hertz_test.go
+++ commons/types/hertz_test.go
@@ -2,24 +2,31 @@ package types
 
 import (
 	"testing"
+	"math"
+	"fmt"
 )
 
-//var c *cache.Cache
-//var db *badger.DB
-//var dbPath = "." + string(os.PathSeparator) + "testdb"
-
-
-////init
-//func init()  {
-//	c = cache.New(CacheTTL, CacheTTL*2)
-//	utils.Info("opening DB...")
-//	opts := badger.DefaultOptions
-//	opts.Dir = dbPath
-//	opts.ValueDir = dbPath
-//	db, _ = badger.Open(opts)
-//}
 
 func TestHerz(t *testing.T) {
 	hertz := Hertz{Db: db}
 	hertz.Merge()
+}
+
+func TestGrowth(t *testing.T) {
+	MaxTTL := 86400  //nbr seconds in a day
+	//MinTTL := 1
+	UppertTxThreshold := 1000
+
+	// 86.4 = 1 ^ gf
+	expGrowthFactor := math.Log2E * float64((MaxTTL / UppertTxThreshold))
+	value := fmt.Sprintf("%v\n", expGrowthFactor)
+	fmt.Printf(value)
+	fmt.Printf("%f\n", 1000 * expGrowthFactor)
+	fmt.Printf("%f\n", 500 * expGrowthFactor)
+	fmt.Printf("%f\n", 250 * expGrowthFactor)
+	fmt.Printf("%f\n", 125 * expGrowthFactor)
+	fmt.Printf("%f\n", 75 * expGrowthFactor)
+	fmt.Printf("%f\n", 25 * expGrowthFactor)
+	fmt.Printf("%f\n", 10 * expGrowthFactor)
+
 }
