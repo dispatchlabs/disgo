2e81086d66e87011fe0f569d623e70650da8c27a
Bob Stevens <Bob@bobs-mbp.lan>
2018-09-05 21:03:44 -0700

    Initial implementation
    Still needs integration, but is demonstratable

---
+++ commons/types/rate_limit.go
@@ -0,0 +1,230 @@
+package types
+
+import (
+	"github.com/dgraph-io/badger"
+	"github.com/patrickmn/go-cache"
+	"encoding/binary"
+	"time"
+	"fmt"
+	"github.com/dispatchlabs/disgo/commons/utils"
+	"encoding/json"
+	"math"
+)
+
+const (
+	EXP_GROWTH = 1.645504582
+)
+
+type RateLimit struct {
+	Address 	string
+	TxRateLimit *TxRateLimit
+	Existing	*AccountRateLimits
+	Db      	*badger.DB
+	Page        string
+}
+
+type TxRateLimit struct {
+	Amount 		int64
+	TxHash  	string
+}
+
+type AccountRateLimits struct {
+	TxHashes	[]string
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//  RateLimit
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+func NewRateLimit(address, txHash, page string, amount int64) (*RateLimit, error) {
+	return &RateLimit {
+		Address:	address,
+		TxRateLimit: &TxRateLimit{amount, txHash},
+		Existing: &AccountRateLimits{make([]string, 0)},
+		Page: page,
+	}, nil
+}
+
+func (this *RateLimit) Set(txn *badger.Txn, cache *cache.Cache) error {
+	existing, err := GetAccountRateLimit(txn, cache, this.Address)
+	if err != nil {
+		utils.Error(err)
+	}
+	if existing != nil {
+		existing.TxHashes = append(existing.TxHashes, this.TxRateLimit.TxHash)
+		this.Existing = existing
+	} else {
+		this.Existing.TxHashes = append(this.Existing.TxHashes, this.TxRateLimit.TxHash)
+	}
+
+	this.cache(cache)
+	err = this.persist(txn)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (this *RateLimit) cache(cache *cache.Cache) {
+	cache.Set(getAccountRateLimitKey(this.Address), this.Existing, TransactionCacheTTL)
+	cache.Set(getTxRateLimitKey(this.TxRateLimit.TxHash), this.TxRateLimit, this.getCurrentTTL())
+}
+
+func (this *RateLimit) persist(txn *badger.Txn) error {
+
+	err := txn.Set([]byte(getAccountRateLimitKey(this.Address)), []byte(this.Existing.string()))
+	if err != nil {
+		return err
+	}
+	err = txn.SetWithTTL([]byte(getTxRateLimitKey(this.TxRateLimit.TxHash)), []byte(this.TxRateLimit.string()), TransactionCacheTTL)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (this RateLimit) ToPrettyJson() string {
+	bytes, err := json.MarshalIndent(this, "", "  ")
+	if err != nil {
+		utils.Error("unable to marshal RateLimit", err)
+		return ""
+	}
+	return string(bytes)
+}
+
+func (this RateLimit) getCurrentTTL() time.Duration {
+	value, err := this.Merge()
+	if err != nil {
+		utils.Error(err)
+	}
+	nbrSeconds := math.Pow(float64(value), EXP_GROWTH)
+	ttl := time.Duration(nbrSeconds) * time.Second
+	utils.Info("Current TTL = ", ttl.String())
+	return ttl
+}
+
+func (this *RateLimit) Merge() (uint64, error) {
+	key := []byte(this.Page)
+	m := this.Db.GetMergeOperator(key, add, 200*time.Millisecond)
+	defer m.Stop()
+
+	m.Add(uint64ToBytes(1))
+
+	res, err := m.Get()
+	if err != nil {
+		return 0, err
+	}
+	result := bytesToUint64(res)
+	utils.Info("Current Count = ", result)
+	return result, nil
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//  Account
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+func getAccountRateLimitKey(address string) string {
+	return fmt.Sprintf("table-ratelimit-account%s", address)
+}
+
+func GetAccountRateLimit(txn *badger.Txn, cache *cache.Cache, address string) (*AccountRateLimits, error) {
+	key := getAccountRateLimitKey(address)
+	value, ok := cache.Get(key)
+	if !ok {
+		item, err := txn.Get([]byte(key))
+		if err != nil {
+			return nil, err
+		}
+		value, err := item.Value()
+		if err != nil {
+			return nil, err
+		}
+		accountRateLimits, err := toAccountRateLimitsFromJson(value)
+		return accountRateLimits, nil
+	}
+	arl := value.(*AccountRateLimits)
+	return arl, nil
+}
+
+func toAccountRateLimitsFromJson(payload []byte) (*AccountRateLimits, error) {
+	arl := &AccountRateLimits{}
+	err := json.Unmarshal(payload, arl)
+	if err != nil {
+		return nil, err
+	}
+	return arl, nil
+}
+
+func (this AccountRateLimits) string() string {
+	bytes, err := json.Marshal(this)
+	if err != nil {
+		utils.Error("unable to marshal AccountRateLimits", err)
+		return ""
+	}
+	return string(bytes)
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//  Transaction
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+func getTxRateLimitKey(hash string) string {
+	return fmt.Sprintf("table-ratelimit-transaction-%s", hash)
+}
+
+func GetTxRateLimit(txn *badger.Txn, cache *cache.Cache, hash string) (*TxRateLimit, error) {
+	key := getTxRateLimitKey(hash)
+	value, ok := cache.Get(key)
+	if !ok {
+		item, err := txn.Get([]byte(key))
+		if err != nil {
+			return nil, err
+		}
+		value, err := item.Value()
+		if err != nil {
+			return nil, err
+		}
+		rateLimit, err := toTxRateLimitFromJson(value)
+		return rateLimit, nil
+	}
+	rateLimit := value.(*TxRateLimit)
+	return rateLimit, nil
+}
+
+func toTxRateLimitFromJson(payload []byte) (*TxRateLimit, error) {
+	txRl := &TxRateLimit{}
+	err := json.Unmarshal(payload, txRl)
+	if err != nil {
+		return nil, err
+	}
+	return txRl, nil
+}
+
+func (this TxRateLimit) string() string {
+	bytes, err := json.Marshal(this)
+	if err != nil {
+		utils.Error("unable to marshal RateLimit", err)
+		return ""
+	}
+	return string(bytes)
+}
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//  Helpers
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+func uint64ToBytes(i uint64) []byte {
+	var buf [8]byte
+	binary.BigEndian.PutUint64(buf[:], i)
+	return buf[:]
+}
+
+func bytesToUint64(b []byte) uint64 {
+	return binary.BigEndian.Uint64(b)
+}
+
+// Merge function to add two uint64 numbers
+func add(existing, new []byte) []byte {
+	return uint64ToBytes(bytesToUint64(existing) + bytesToUint64(new))
+}
---
+++ commons/types/rate_limit_test.go
@@ -0,0 +1,87 @@
+package types
+
+import (
+	"testing"
+	"math"
+	"fmt"
+	"github.com/dispatchlabs/disgo/commons/utils"
+	"time"
+)
+
+
+func TestRateLimit(t *testing.T) {
+	rateLimit := RateLimit{Db: db}
+	rateLimit.Merge()
+}
+
+
+func TestRateLimitStorage(t *testing.T) {
+	hash1 := "44197cc2241ad63b66039e15a85168857272fe1625ed39999972edcdfcbc1bbd"
+	hash2 := "44197cc2241ad63b66039e15a85168857272fe1625ed39999972edcdfcbc1bbe"
+	page := "page-1"
+	address := "test"
+	rateLimit, err := NewRateLimit(address, hash1, page, 5)
+	rateLimit.Db = db
+	if err != nil {
+		t.Error(err)
+	}
+	addRateLimit(rateLimit)
+	time.Sleep(3 *time.Second)
+	rateLimit, err = NewRateLimit(address, hash2, page,10)
+	rateLimit.Db = db
+	if err != nil {
+		t.Error(err)
+	}
+	addRateLimit(rateLimit)
+	//fmt.Printf("%s\n", rateLimit.ToPrettyJson())
+
+}
+
+func addRateLimit(rateLimit *RateLimit) {
+	txn := db.NewTransaction(true)
+	defer txn.Discard()
+	rateLimit.Set(txn, c)
+
+	txRateLimit, err := GetTxRateLimit(txn, c, rateLimit.TxRateLimit.TxHash)
+	if err != nil {
+		utils.Error(err)
+	}
+	addrsRateLimit, err := GetAccountRateLimit(txn, c, rateLimit.Address)
+	if err != nil {
+		utils.Error(err)
+	}
+	fmt.Printf("%s\n", txRateLimit.string())
+	fmt.Printf("%s\n", addrsRateLimit.string())
+
+	var totalDeduction int64
+	for _, hash := range addrsRateLimit.TxHashes {
+		utils.Info("Getting Hash: ", hash)
+		txrl, err := GetTxRateLimit(txn, c, hash)
+		if err != nil {
+			utils.Error(err)
+		}
+		if txrl != nil {
+			totalDeduction += txrl.Amount
+			utils.Info(txrl.string())
+		}
+	}
+	utils.Info("\nTotal Hertz Deduction from account = ", totalDeduction)
+}
+
+func TestGrowth(t *testing.T) {
+	//MaxTTL := 86400.0  //nbr seconds in a day
+	//MinTTL := 1
+	UppertTxThreshold := 1000.0
+
+	printValue(1.0)
+	printValue(10.0)
+	printValue(100.0)
+	printValue(250.0)
+	printValue(500.0)
+	printValue(UppertTxThreshold)
+
+}
+
+func printValue(value float64) {
+	fmt.Printf("%f\n", math.Pow(value, EXP_GROWTH))
+}
