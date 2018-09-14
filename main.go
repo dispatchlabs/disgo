1911b7069c77d742203e6fdedc3b088f4494a753
David Hutchings <chefslide@gmail.com>
2018-09-13 21:33:48 -0700

    Require Go 1.10 in main.go

--- main.go
+++ main.go
@@ -14,6 +14,9 @@
  *    You should have received a copy of the GNU General Public License
  *    along with the Disgo library.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+// +build go1.10
+
 package main
 
 import (
